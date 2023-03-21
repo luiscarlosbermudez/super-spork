c6f9e90f54ecc2819f23d8e1b3796e96d21e260d
Luis Bermudez <luiscarlos.bermudez@mq.edu.au>
2023-03-21 11:54:06 +1100

    Making a CLI

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -6,6 +6,7 @@ This module generates a catalog of stars near Andromeda, with their positions in
 
 from random import uniform
 from math import pi, cos
+import argparse
 
 NSRC = 1_000
 
@@ -51,18 +52,51 @@ def make_stars(RA,DEC,num_stars):
         decs.append(DEC + uniform(-1,1))
     return (ras,decs)
 
-def main():
 #if __name__ == '__main__':
-    RA,DEC = get_radec()
-    ras,decs = make_stars(RA,DEC,NSRC)
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
+def main():
+    if __name__ == "__main__":
+        parser = skysim_parser()
+        options = parser.parse_args()
+    # if ra/dec are not supplied the use a default value
+    if None in [options.ra, options.dec]:
+        ra, dec = get_radec()
+    else:
+        ra = options.ra
+        dec = options.dec
+
+    ras, decs = make_positions(ra,dec)
     # now write these to a csv file for use by my other program
-    #f = open('catalog.csv','w')
-    with open('catalog.csv', 'w', encoding='utf-8') as f:
-        # code that uses the file object
+    with open(options.out,'w') as f:
         print("id,ra,dec", file=f)
         for i in range(NSRC):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+    print(f"Wrote {options.out}")
+##    RA,DEC = get_radec()
+##    ras,decs = make_stars(RA,DEC,NSRC)
+    # now write these to a csv file for use by my other program
+    #f = open('catalog.csv','w')
+##    with open('catalog.csv', 'w', encoding='utf-8') as f:
+        # code that uses the file object
+##        print("id,ra,dec", file=f)
+##       for i in range(NSRC):
+##            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
             #print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
 
-if __name__ == '__main__':
-    main()
